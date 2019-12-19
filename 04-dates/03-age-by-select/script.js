bb32852e46210e68bc9affb4430f836745c5831d
Becode <stevekeersma@gmail.com>
2019-12-19 10:31:49 +0100

    solved calculating of age in 04/03

--- 04-dates/03-age-by-select/script.js
+++ 04-dates/03-age-by-select/script.js
@@ -18,11 +18,28 @@
         currentMonths=  document.getElementById("dob-month").value;
         currentYear=    document.getElementById("dob-year").value;
 
+        //zet input om in een string van getallen, split deze om een datum te krijgen
         let dateString = currentDay + "/" + currentMonths + "/" + currentYear;
         let toDate = new Date(dateString);
         dateString = toDate.getFullYear() + "/" + toDate.getMonth() + "/" + toDate.getDate();
         console.log(toDate);
 
+        //function for calculating birthday
+        function getAge(birthdateString)
+        {
+            var today = new Date();
+            var birthDate = new Date(birthdateString);
+            //compares year of today with year of birthdate
+            var age = today.getFullYear() - birthDate.getFullYear();
+            var m = today.getMonth() - birthDate.getMonth();
+            //checks if birthdaymonth passed
+            if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate()))
+            {
+                age--;
+            }
+            return age;
+        }
+        alert(getAge(toDate))
 
     });
 
